0a91c349da5b671f88ea696684bb661ef1a5844c
Vasistan Shakkaravarthi <vasistan_shakkaravarthi@trimble.com>
2025-07-14 14:06:06 +0530

    Clean up template specific tests

--- test/QuantityTakeoffOrchestratorService.SmokeTests/Fixtures/SmokeFixture.cs
+++ test/QuantityTakeoffOrchestratorService.SmokeTests/Fixtures/SmokeFixture.cs
@@ -18,7 +18,6 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using QuantityTakeoffOrchestratorService.Controllers.Students.Models.Documents;
 using QuantityTakeoffOrchestratorService.Test.Common.Clients;
 using QuantityTakeoffOrchestratorService.Test.Common.Fixtures;
 using Flurl.Http;
@@ -38,21 +37,11 @@ namespace QuantityTakeoffOrchestratorService.SmokeTests.Fixtures;
 /// </summary>
 public class SmokeFixture : ProgramFixture, IAsyncLifetime
 {
-    private FilterDefinitionBuilder<StudentDocument> _filter = default!;
-    private IMongoCollection<StudentDocument> _studentCollection = default!;
-    public StudentClient StudentClient { get; set; } = default!;
 
     public new async Task InitializeAsync()
     {
         await base.InitializeAsync();
 
-        var mongoDbService = Services.GetRequiredService<IMongoDbService>();
-        mongoDbService.Database.EnsureCollectionExists<StudentDocument>("student");
-        _studentCollection = mongoDbService.Database.GetCollection<StudentDocument>("student");
-        _filter = Builders<StudentDocument>.Filter;
-
-        var client = GetHttpClient();
-        StudentClient = new StudentClient(new FlurlClient(client));
     }
 
     public new Task DisposeAsync() => ProgramFixture.DisposeAsync();
@@ -62,18 +51,11 @@ public class SmokeFixture : ProgramFixture, IAsyncLifetime
     /// </summary>
     public async Task RestartAsync()
     {
-        var filter = _filter.Regex(x => x.Email, new BsonRegularExpression($".*{StudentFixture.PseudoProvider}"));
-        await _studentCollection.DeleteManyAsync(filter);
+        return Task.CompletedTask;
     }
 
     private HttpClient GetHttpClient()
     {
-        if (!TestFixture.IsDeployment)
-        {
-            return CreateClient();
-        }
-
-        var baseUrl = Services.GetRequiredService<IOptions<DeploymentSettings>>().Value.BaseUrl!;
-        return new HttpClient { BaseAddress = new Uri(baseUrl) };
+       
     }
 }
